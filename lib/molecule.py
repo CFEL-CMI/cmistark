05495ae5a9161c00dd01f1e49faaef3c385b9902
Jochen Küpper <jochen.kuepper@cfel.de>
2012-08-13 20:58:53 +0000

    formatting, documentation
    git-svn-id: https://svnsrv.desy.de/desy/CMIstark/branches/Summerstudent2012@90 6928c15e-da89-41d0-bb8b-734ed9401e8f

--- lib/molecule.py
+++ lib/molecule.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8; fill-column: 120 -*-
 #
 # This file is part of JK Python extensions
-# Copyright (C) 2008,2009 Jochen Küpper <software@jochen-kuepper.de>
+# Copyright (C) 2008,2009,2012 Jochen Küpper <software@jochen-kuepper.de>
 #
 # This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
 # License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
