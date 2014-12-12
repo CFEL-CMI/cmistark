682868069e55db5bd7c83f223ad98f00dd4b0098
Jochen Küpper <jochen.kuepper@cfel.de>
2014-12-12 09:22:13 +0100

    python 2 compatibility

--- cmistark/starkeffect.py
+++ cmistark/starkeffect.py
@@ -14,6 +14,7 @@
 #
 # You should have received a copy of the GNU General Public License along with this program. If not, see
 # <http://www.gnu.org/licenses/>.
+from __future__ import print_function
 
 __author__ = "Jochen Küpper <jochen.kuepper@cfel.de>"
 
