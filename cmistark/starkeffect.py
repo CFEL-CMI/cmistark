71390bb09026383c75c3b554d07fe83241f42fc1
Jochen Küpper <jochen.kuepper@cfel.de>
2015-09-17 20:18:04 +0200

    we require Python3 by now

--- cmistark/starkeffect.py
+++ cmistark/starkeffect.py
@@ -14,8 +14,6 @@
 #
 # You should have received a copy of the GNU General Public License along with this program. If not, see
 # <http://www.gnu.org/licenses/>.
-from __future__ import print_function
-
 __author__ = "Jochen Küpper <jochen.kuepper@cfel.de>"
 
 # really use scipy as numpy, so we are sure we use Fortran codes of eigvalsh and dgemm
